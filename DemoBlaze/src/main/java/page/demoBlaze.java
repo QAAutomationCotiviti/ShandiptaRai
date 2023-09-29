9ed4c62ae9172b60986741f1655bf95014afbea0
shandipta-chamling <er.sndpta@live.ocm>
2023-09-29 15:42:12 +0545

    DemoBlazer committed on 20230929 for 2nd time.

--- DemoBlaze/src/main/java/page/demoBlaze.java
+++ DemoBlaze/src/main/java/page/demoBlaze.java
@@ -1,8 +1,6 @@
 package page;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import pageElements.signUp;
 import utilities.finalVariables;
 import utilities.utility;
@@ -14,26 +12,33 @@ public class demoBlaze {
     static signUp signUp = new signUp(driver);
 
     public static void main(String[] args) {
-        driver.manage().window().maximize();
-        driver.get("https://www.demoblaze.com/");
-        getSignUp("","");
-        getSignUp("dfsdfsdfsdfsdsdsdg","sdsfsfsdfsdf");
-        getSignUp("dfsdfsdfsdfsdsdsdg","sdsfsfsdfsdf");
+        try {
+            driver.manage().window().maximize();
+            driver.get("https://www.demoblaze.com/");
+            getSignUp("", "");
+//            getSignUp("dfsdfsdfsdfsdsdsdg", "sdsfsfsdfsdf");
+//            getSignUp("dfsdfsdfsdfsdsdsdg", "sdsfsfsdfsdf");
+        }
+        catch (ElementNotInteractableException| NoAlertPresentException e){
+            System.out.println("Error: " +e);
+
+        }
     }
 
     public static void getSignUp(String username, String password){
-        driver.findElements(signUp.clickSignUp());
+        driver.findElement(signUp.clickSignUp()).click();
         By[] getSignUpInput = signUp.getSignUpInput();
         By[] getSignUpButtton = signUp.getSignUpButton();
-        WebElement signUpModal = util.waitForElementTObeLocated(driver,15,signUp.signUpModal());
-        WebElement uname = util.waitForElementTObeLocated(driver,15,getSignUpInput[0]);
+        WebElement signUpModal = util.waitForElementTObeLocated(driver,20,signUp.signUpModal());
+        System.out.println(signUpModal.isDisplayed());
+        WebElement uname = util.waitForElementTObeLocated(driver,20,getSignUpInput[0]);
         uname.clear();
         uname.sendKeys(username);
-        WebElement pword = util.waitForElementTObeLocated(driver,15,getSignUpInput[1]);
+        WebElement pword = util.waitForElementTObeLocated(driver,20,getSignUpInput[1]);
         pword.clear();
         pword.sendKeys(password);
         signUpModal.findElement(getSignUpButtton[1]).click();
-        util.checkAlert(15,driver);
+        util.checkAlert(20,driver);
         if(finalVariables.signUpSuccess.equals(util.getAlert(driver).getText())){
             System.out.println(util.getAlert(driver).getText());
             util.getAlert(driver).accept();
@@ -42,7 +47,7 @@ public class demoBlaze {
             util.getAlert(driver).accept();
             signUpModal.findElement(getSignUpButtton[0]).click();
 
-        }
+        }driver.navigate().refresh();
 
 
 
--- DemoBlaze/src/main/java/pageElements/signUp.java
+++ DemoBlaze/src/main/java/pageElements/signUp.java
@@ -6,11 +6,11 @@ import org.openqa.selenium.WebDriver;
 public class signUp {
     private final WebDriver driver;
     private final By signupLink =By.id("signin2");
-    private final By signUpModal =By.xpath("//[@class='signInModal']//div[@class='modal-content']");
-    private final By signupUsername = By.xpath("//[@class='signInModal']//input[@id='sign-username']");
-    private final By signupassword = By.xpath("//[@class='signInModal']//input[@id='sign-password']");
-    private final By cancleButton = By.xpath("//[@class='signInModal']//button[@class='btn btn-secondary']");
-    private final By signupButton = By.xpath("//[@class='signInModal']//button[@class='btn btn-primary']");
+    private final By signUpModal =By.xpath("//div[@id='signInModal']//div[@class='modal-content']");
+    private final By signupUsername = By.xpath("//div[@id='signInModal']//input[@id='sign-username']");
+    private final By signupassword = By.xpath("//div[@id='signInModal']//input[@id='sign-password']");
+    private final By cancleButton = By.xpath("//div[@id='signInModal']//button[@class='btn btn-secondary']");
+    private final By signupButton = By.xpath("//div[@id='signInModal']//button[@class='btn btn-primary']");
 
     public signUp(WebDriver driver){
         this.driver=driver;
